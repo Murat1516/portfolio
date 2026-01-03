91dbdf672d1fd025358f6608f82c6a7a1381feb3
Murat1516 <muratdasdemir5@gmail.com>
2026-01-03 19:41:35 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,19 @@
-self.addEventListener("install",e=>{
-  e.waitUntil(caches.open("portfolio-v1").then(c=>c.addAll(["./"])));
+const CACHE_NAME = 'portfolio-cache-v1';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
-self.addEventListener("fetch",e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
